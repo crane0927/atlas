7bf8621e0a9b8f34cf33f6fb8bed96c0caa3d160
liuhuan <crane0927@163.com>
2026-01-29 16:23:45 +0800

    feat(redis): 添加 Redis 自动配置类，更新 Nacos 配置组为 DEFAULT_GROUP，删除不再使用的环境配置文件

---
+++ atlas-common/atlas-common-infra/atlas-common-infra-redis/src/main/java/com/atlas/common/infra/redis/config/RedisAutoConfiguration.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025 Atlas. All rights reserved.
+ */
+package com.atlas.common.infra.redis.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Redis 自动配置类。
+ *
+ * <p>为 Spring Boot 应用自动注册 Redis 配置，包括：
+ *
+ * <ul>
+ *   <li>RedisTemplate Bean 配置（统一的序列化方式）
+ *   <li>RedisKeyBuilder 初始化
+ *   <li>CacheUtil 组件注册
+ * </ul>
+ *
+ * <p>当依赖缺失时，自动配置不会生效，服务回退到默认 Redis 配置。
+ *
+ * @author Atlas Team
+ * @since 1.0.0
+ */
+@AutoConfiguration
+@ConditionalOnClass(RedisTemplate.class)
+@Import({RedisConfig.class})
+@ComponentScan(basePackages = "com.atlas.common.infra.redis")
+public class RedisAutoConfiguration {}
